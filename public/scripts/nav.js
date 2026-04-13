a95e0457caafcb311552823815f7b32361cda649
Chris0925-coder <christianguardia@outlook.es>
2026-04-13 01:03:49 -0500

    0

---
+++ public/scripts/nav.js
@@ -0,0 +1,20 @@
+const date = new Date();
+const footerDate = document.querySelector('.date');
+
+const menu = document.querySelector('.btn-menu');
+const nav = document.querySelector('.nav-bar')
+
+menu.addEventListener('click', (e) => {
+	nav.classList.toggle('show');
+
+	navListen();
+});
+
+function navListen() {
+	nav.addEventListener('click', (e) => {
+		if(e.target.tagName == 'A') nav.classList.remove('show');
+	})
+}
+
+
+footerDate.append(date.getFullYear());
---
+++ public/scripts/video.js
@@ -0,0 +1,24 @@
+const video = document.querySelector('#video');
+const item = video.querySelector('.item:nth-child(2)');
+
+let newVideo = document.createElement('div');
+
+newVideo.setAttribute('class', 'container');
+
+let videos = [
+	'AQOJpErycrFY2KdAtVMRH_zI1pZHzBD-hhSRgxAr4S-qiaMY0VkKub6UY0XrtWJQyDb18L955tuj24hsxOwP3U8v_.mp4',
+	'AQPCKMPX2OUQMQRhK4NeEHoyDM2hyLunmQxG3NxbhZCKvW9oaZ2h9s9shUPxdAPTfTBdDKOVj_5MZtMxJCF1tZ3c8U7qE3l2.mp4',
+	'main.mp4'
+];
+
+videos.forEach(v => {
+
+	newVideo.innerHTML += `
+		<video controls>
+			<source src="../public/video/${v}">
+		</video>
+	`;
+
+	video.insertBefore(newVideo, item);
+})
+
