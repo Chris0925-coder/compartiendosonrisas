11321870f8343fc524ea5c3652a66e12476eb0db
Chris0925-coder <christianguardia@outlook.es>
2026-05-05 17:26:05 -0500

    0

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,8 +15,8 @@ let newDiv = document.createElement('div');
 newDiv.setAttribute('class', 'new-div');
 
 let images = [
-	`<picture><img src="public/img/entrega_de_alimentos/IMG-20260417-WA0013.jpg"></picture>
-	<picture><img src="public/img/entrega_de_alimentos/new_img_a.jpg"></picture>
+	`<picture><img src="public/img/entrega_de_alimentos/IMG-20260417-WA0013.jpg" alt="entrega_de_alimentos"></picture>
+	<picture><img src="public/img/entrega_de_alimentos/new_img_a.jpg" alt="entrega_de_alimentos"></picture>
 	`,
 	// "new_img_a.jpg",
 	// "new_img_c.jpg",
@@ -26,24 +26,24 @@ let images = [
 ];
 
 let imagesB = [
-	`<picture><img src="public/img/actividades_sociales/644963196_122123658255082145_7861592502207344555_n_.jpg"></picture>
-	<picture><img src="public/img/actividades_sociales/646377186_122123658339082145_8781551563745798074_n_.jpg"></picture>
-	<picture><img src="public/img/actividades_sociales/646454457_122123658471082145_5059191120632262630_n_.jpg"></picture>`,
-	`<picture><img src="public/img/actividades_sociales/646468067_122123658381082145_4855597248081070575_n_.jpg"></picture>
+	`<picture><img src="public/img/actividades_sociales/644963196_122123658255082145_7861592502207344555_n_.jpg" alt="actividades_sociales"></picture>
+	<picture><img src="public/img/actividades_sociales/646377186_122123658339082145_8781551563745798074_n_.jpg" alt="actividades_sociales"></picture>
+	<picture><img src="public/img/actividades_sociales/646454457_122123658471082145_5059191120632262630_n_.jpg" alt="actividades_sociales"></picture>`,
+	`<picture><img src="public/img/actividades_sociales/646468067_122123658381082145_4855597248081070575_n_.jpg" alt="actividades_sociales"></picture>
 	<picture><img src="public/img/actividades_sociales/new_img_c.jpg"></picture>`
 ];
 
 let imagesC = [
-	`<picture><img src="public/img/voluntariado/646407219_122123658513082145_2299101496271544037_n_.jpg"></picture>
-	<picture><img src="public/img/voluntariado/646716339_122123658297082145_1618010219896545606_n_.jpg"></picture>`,
-	`<picture><img src="public/img/voluntariado/new_img_b.jpg"></picture>
-	<picture><img src="public/img/voluntariado/new_img_d.jpg"></picture>`,
+	`<picture><img src="public/img/voluntariado/646407219_122123658513082145_2299101496271544037_n_.jpg" alt="voluntariado"></picture>
+	<picture><img src="public/img/voluntariado/646716339_122123658297082145_1618010219896545606_n_.jpg" alt="voluntariado"></picture>`,
+	`<picture><img src="public/img/voluntariado/new_img_b.jpg" alt="voluntariado"></picture>
+	<picture><img src="public/img/voluntariado/new_img_d.jpg" alt="voluntariado"></picture>`,
 ];
 
 let imagesD = [
-	`<picture><img src="public/img/hospitales/645302926_122123658573082145_7812428749982802116_n_.jpg"></picture>
-	<picture><img src="public/img/hospitales/1000019341.jpg"></picture>
-	<picture><img src="public/img/hospitales/IMG-20260417-WA0005.jpg"></picture>`,
+	`<picture><img src="public/img/hospitales/645302926_122123658573082145_7812428749982802116_n_.jpg" alt="hospitales"></picture>
+	<picture><img src="public/img/hospitales/1000019341.jpg" alt="hospitales"></picture>
+	<picture><img src="public/img/hospitales/IMG-20260417-WA0005.jpg" alt="hospitales"></picture>`,
 ];
 
 let title = [
